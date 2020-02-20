450e6ad08f31100ac7fa97672e6082f142992f0b
Miguel Romero <43483471+romeromig@users.noreply.github.com>
2020-02-20 17:17:54 -0600

    Update hitter_table.py

--- scripts/hitter_table.py
+++ scripts/hitter_table.py
@@ -4,8 +4,8 @@ import sys
 if len(sys.argv[1:]) == 0:
     print(
 """
-Agrupar tablas generadas por hitter.py
-usage: python3 hitter_table.py hout_names_list proyecto
+Merge tables built with the hitter.py script.
+usage: python3 hitter_table.py hout_names_list project
 Miguel Romero github.com/romeromig
 """
 )
@@ -29,14 +29,14 @@ handle = open(argvs[1] +'.tmp1','r')
 md5s = handle.read().split('\n')[:-1]
 handle.close()
 
-proyect_dict = {} #generar tabla final vacia
+proyect_dict = {} # final table
 for md5 in md5s:
     proyect_dict[md5] = placeholder.copy()
 
-print('Construyendo tabla')
+print('Building table.')
 index = 0
 for infile in infiles:
-    print('Agregando archivo '+ infile)
+    print('Loading file: '+ infile)
     handle = open(infile,'r')
     sample = handle.read().split('\n')[:-1]
     handle.close()
@@ -45,10 +45,12 @@ for infile in infiles:
         proyect_dict[linesep[0]][index] = linesep[1]
     index += 1
 
-outfile = open(argvs[1]+'.tsv','w') #guardar en un archivo de texto
+outfile = open(argvs[1]+'.tsv','w') # save in a text file
 outfile.write('md5\t'+ '\t'.join(infiles) +'\n')
 for md5 in proyect_dict.keys():
     outfile.write(md5 +'\t'+ '\t'.join(proyect_dict[md5]) +'\n')
 outfile.close()
-print('Hecho. Tabla final guardada en '+ argvs[1] +'.tsv')
-print('Se recomienda borrar el archivo temporal: rm '+ argvs[1] +'.tmp1')
+
+os.system('rm '+ argvs[1] +'.tmp1')
+
+print('Done. Final table saved in '+ argvs[1] +'.tsv')
