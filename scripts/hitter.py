be5269dac272afbf26fbf36d22392a8894466a9a
Miguel Romero <43483471+romeromig@users.noreply.github.com>
2020-02-20 17:00:42 -0600

    Update hitter.py

--- scripts/hitter.py
+++ scripts/hitter.py
@@ -4,8 +4,8 @@ import sys
 if len(sys.argv[1:]) == 0:
     print(
 """
-Generar tablas de secuencias anotadas con conteos basados en mapeo de lecturas
-usage: python3 hitter.py annotation.txt hitfile.txt muestra
+Build a table of annotated sequences and their mapped read abundance.
+usage: python3 hitter.py annotation.txt hitfile.txt sample
 Miguel Romero github.com/romeromig
 """
 )
@@ -13,59 +13,59 @@ Miguel Romero github.com/romeromig
 
 infiles = sys.argv[1:]
 
-print('Cargando archivos')
+print('Loading files.')
 
-# cargar tabla de anotacion
+# load annotation table
 handle = open(infiles[0])
 annot = handle.read().split('\n')[:-1]
 handle.close()
 
-# cada secuencia punta a su anotacion
+# each sequence points to its annotation
 annot_dict = {} 
 for line in annot:
     linesep = line.split('\t')
     annot_dict[linesep[0]] = linesep[-1]
 
-# cargar tabla de mapeo
+# load read abundance table
 handle = open(infiles[1])
 hits = handle.read().split('\n')[:-1]
 handle.close()
 
-# cada secuencia apunta a su abundancia
+# each sequence points to its abundance
 hit_dict = {}
 for line in hits:
     linesep = line.split(' ')[-2:]
     hit_dict[linesep[1]] = int(linesep[0])
 
-print('Buscando secuencias anotadas y mapeadas')
+print('Looking for annotated and mapped sequences.')
 chidas = []
 for seqid in list(annot_dict.keys()):
     if seqid in list(hit_dict.keys()):
         chidas.append(seqid)
 
-print('Creando tabla')
+print('Building table.')
 
-# sumar la abundancia por cada anotacion
+# create a row for each reference ID
 annotcount_dict = {} 
 for seqid in chidas:
     annotcount_dict[annot_dict[seqid]] = 0
 
-print('Sumando abundancias')
+print('Adding read counts')
 
 for seqid in chidas:
     annotcount_dict[annot_dict[seqid]] += hit_dict[seqid]
 
-print('Ordenando')
+print('Sorting')
 
 rownames = list(annotcount_dict.keys())
 rownames.sort()
 
-print('Escribiendo tabla')
+print('Writing table.')
 
-# guardar en un archivo de texto
+# saving in a text file
 outfile = open(infiles[2]+'.hout','w') 
 for annotid in rownames:
     outfile.write(annotid +'\t'+ str(annotcount_dict[annotid]) +'\n')
 outfile.close()
 
-print('Hecho. Tabla final guardada en '+ infiles[2] +'.hout')
+print('Done. Final table saved in '+ infiles[2] +'.hout')
